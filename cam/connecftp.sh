4a8fb81bb2cfce68554598afc2c4e139dd5f193c
yapa69 <timothee.birckel@gmail.com>
2014-03-28 19:10:30 +0100

    Update connecftp.sh

--- cam/connecftp.sh
+++ cam/connecftp.sh
@@ -3,10 +3,13 @@
 # usage : this_script fulltemp
 fichierjpg="/img/cam$(date +"%m%d%Y%H").jpg"
 fichiermp4="/img/cam$(date +"%m%d%Y%H").mp4"
+login=""
+server=""
+site=""
 
 if [[ $1 = "fulltemp" ]]
 then
-scp -i /home/pi/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/pi/cam/img/lastvideo.mp4 tbirckel@kim.terrier.im:echartet$fichiermp4
+scp -i /home/pi/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/pi/cam/img/lastvideo.mp4 $login@$server:$site$fichiermp4
 scp -i /home/pi/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/pi/cam/RRDtemp/mhour.png tbirckel@kim.terrier.im:echartet/img/mhour.png
 scp -i /home/pi/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/pi/cam/RRDtemp/mday.png tbirckel@kim.terrier.im:echartet/img/mday.png
 scp -i /home/pi/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /home/pi/cam/RRDtemp/mweek.png tbirckel@kim.terrier.im:echartet/img/mweek.png
