e7bb3bc5c6ce6e2e66b95e7234d658d186bfea57
ldvc <ludovic+github@terrier.im>
2014-04-02 13:16:54 +0200

    Uniformize bash script

--- cam/maintenance.sh
+++ cam/maintenance.sh
@@ -1,20 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ############################################
 #
-# Maintenance fo reverse SSH (NAT connexion)
+# Maintenance for reverse SSH (NAT connexion)
 #
 # Ludovic
 ############################################
 
-#/bin/bash /home/pi/cam/ppp-off
-date >>/var/log/SSHmaintenance.log
-kill -9 `ps aux | grep "[N]R 22222" | awk '{print $2}'` &>>/var/log/SSHmaintenance.log
-/bin/bash /home/pi/cam/ppp-on &>>/var/log/SSHmaintenance.log
+# Exit as soon as a command does not return 0
+set -e
+
+# Get variables from configuration file
+source conf.sh
+
+#/bin/bash ${HOME_DIR}/ppp-off
+kill -9 `ps aux | grep "[N]R 22222" | awk '{print $2}'` &
+$BASH_PATH ${HOME_DIR}/ppp-on &
 sleep 30
 
-ssh -i /home/pi/.ssh/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -NR 22222:localhost:22 tbirckel@kim.terrier.im &
+ssh -i ${L_SSH_KEY} ${SSH_OPTS} -NR 22222:localhost:22 ${R_USER}@${R_SERVER} &
 sleep $1
-kill -9 `ps aux | grep "[N]R 22222" | awk '{print $2}'` &>>/var/log/SSHmaintenance.log
-printf "Fin maintenance ok\n" >> /var/log/SSHmaintenance.log
-/bin/bash /home/pi/cam/ppp-off
+kill -9 `ps aux | grep "[N]R 22222" | awk '{print $2}'` &
+$BASH_PATH ${HOME_DIR}/ppp-off
