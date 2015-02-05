d18675e6bd25a6bad4be477f3830c7d74f4499df
yapa69 <timothee.birckel@gmail.com>
2015-02-05 19:25:19 +0100

    Update initconnec.sh

--- cam/initconnec.sh
+++ cam/initconnec.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash
+### BEGIN INIT INFO
+# Provides:          Init3GWebcam
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
 
-############################################
-#
-# @boot: Date synch + logs sent to server
-#
-# Tim
-############################################
 
 # Exit as soon as a command does not return 0
 set -e
